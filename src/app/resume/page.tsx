c70e5441190ce3b7cb3b8a868e74a7a545472c4d
biggestyoms <165834284+biggestyoms@users.noreply.github.com>
2025-04-08 15:38:34 -0400

    first commit

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-full pt-[9dvh] '>
+      <div >
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default page
